33d37dd4b051e166a5da7a405c736f1aa1bd2d17
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2017-11-23 21:59:43 +1100

    First commit

---
+++ jumpStatements_break/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace jumpStatements_break
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            for (i = 0; i < 5; i++)
+            {
+                Console.WriteLine("i = {0}", i);
+
+                if (i == 5)
+                    break; /* The break keyword causes the program
+                to exit a loop prematurely when a certain condition
+                is me.*/ 
+            }
+        }
+    }
+}
